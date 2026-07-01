8e2b0307fd92e19656a003d2767c2e26817b8c72
Student <student@example.com>
2026-07-01 13:39:58 +0000

    HoT: initial revision

---
+++ HeadsOrTails.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class HeadsOrTails {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int heads = 0;
+        int tails = 0;
+
+        System.out.println("Tossing a coin...");
+        for (int i = 1; i <= 3; i++) {
+            boolean isHeads = random.nextBoolean();
+            String result = isHeads ? "Heads" : "Tails";
+            if (isHeads) {
+                heads++;
+            } else {
+                tails++;
+            }
+            System.out.println("Round " + i + ": " + result);
+        }
+
+        System.out.println("Heads: " + heads + ", Tails: " + tails);
+    }
+}
